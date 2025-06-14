5d3bea3b0a9d1f596147c312d6654ad46ecfd911
duck <ryane.huang123@gmail.com>
2025-06-14 18:07:29 -0400

    index

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ app.get('/get-stream-url', async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
